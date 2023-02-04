a4249700fdb365bd475ce0c3671d528a55bcb1de
Xerro <96697574+Xerro24@users.noreply.github.com>
2023-02-04 10:39:38 -0800

    khibgfnhp;ghf

--- My project/Assets/Task.cs
+++ My project/Assets/Task.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class Task : MonoBehaviour
 {
-    public int priority = 0;
-    public string Name = 0;
-    public float Time = 0;
-    public string description = 0;
-    public int priority = 0;
+    //public int priority = 0;
+    //public string Name = 0;
+    //public float Time = 0;
+    //public string description = 0;
+    //public int priority = 0;
     // Start is called before the first frame update
     void Start()
     {
