b699d5b6c770af556ef18731a1bc628aaa67982e
Alex De La Chica <80286400+Alexdlc5@users.noreply.github.com>
2023-02-05 09:44:39 -0800

    day display finished

--- My project/Assets/Calander/Day_Manager.cs
+++ My project/Assets/Calander/Day_Manager.cs
@@ -2,14 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 
 public class Day_Manager : MonoBehaviour
 {
+    public TextMeshProUGUI display;
     [SerializeField]private int current_day = 1;
     public Calendar_Main calender_main;
     public void setCurrentDay(int day)
     {
         current_day = day;
+        display.text = day.ToString();
     }
 }
